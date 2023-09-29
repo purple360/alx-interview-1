c04126c24726ccbaabe1272a6aeab648875436ea
purple360 <64763854+purple360@users.noreply.github.com>
2023-09-29 01:32:29 +0000

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,28 +1,16 @@
-#!/usr/bin/env python3
-from typing import List
-
-
-def pascal_triangle(n: int) -> List[list]:
-    '''
-    Pascal triangle
-    '''
+def pascal_triangle(n):
     if n <= 0:
         return []
 
-    if n == 1:
-        return [[1]]
-
-    if n == 2:
-        return [[1], [1, 1]]
-
-    triangle = [[1], [1, 1]]
-
-    for i in range(2, n):
-        temp = [1, 1]
-        for j in range(0, len(triangle[-1])-1):
-            a = triangle[-1][j]
-            b = triangle[-1][j+1]
-            temp.insert(-1, a + b)
-        triangle.append(temp)
+    triangle = []
+    for i in range(n):
+        row = [1]  # First element in each row is always 1
+        if triangle:  # If there are previous rows
+            prev_row = triangle[-1]
+            for j in range(1, i):
+                row.append(prev_row[j - 1] + prev_row[j])
+            row.append(1)  # Last element in each row is always 1
+        triangle.append(row)
 
     return triangle
+    
